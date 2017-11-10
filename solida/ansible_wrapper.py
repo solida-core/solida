12edaee5e30c4f3bd7a3700c15b6a2f7e814a7a5
Gianmauro Cuccuru <gmauro@crs4.it>
2017-11-10 15:21:27 +0100

    move to Ansible 2.4.1

--- solida/ansible_wrapper.py
+++ solida/ansible_wrapper.py
@@ -2,8 +2,8 @@ import os.path
 from collections import namedtuple
 
 from ansible.parsing.dataloader import DataLoader
-from ansible.vars import VariableManager
-from ansible.inventory import Inventory
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager as Inventory
 from ansible.executor.playbook_executor import PlaybookExecutor
 
 
@@ -24,24 +24,22 @@ class AnsibleWrapper(object):
 
             return extra_vars
 
-        variable_manager = VariableManager()
         loader = DataLoader()
 
         Options = namedtuple('Options',
                              ['connection', 'forks', 'become', 'become_method',
-                              'become_user', 'check', 'listhosts', 'listtasks',
-                              'listtags', 'syntax', 'module_path'])
+                              'become_user', 'diff', 'check', 'listhosts',
+                              'listtasks', 'listtags', 'syntax', 'module_path'])
         options = Options(connection='local', forks=100, become=None,
-                          become_method=None, become_user=None,
+                          become_method=None, become_user=None, diff=False,
                           check=False, listhosts=False, listtasks=False,
                           listtags=False, syntax=False, module_path="")
 
         passwords = dict()
 
         inventory = Inventory(loader=loader,
-                              variable_manager=variable_manager,
-                              host_list=[host])
-        variable_manager.set_inventory(inventory)
+                              sources=[host])
+        variable_manager = VariableManager(loader=loader, inventory=inventory)
 
         here = os.path.abspath(os.path.dirname(__file__))
         playbook_list = [os.path.join(here, playbooks_path,
